05b96e3bbf185784fc2ed8606d602f660971f749
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-12-23 21:34:27 -0300

    feat: add helper for queryselector and fetch json data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
-const modal = document.querySelector('.modal');
+// Helpers
+const select = (el, isAll = false) => {
+  if (typeof el === 'string') {
+    return isAll ? document.querySelectorAll(el) : document.querySelector(el);
+  }
+
+  return el;
+};
+
+const strRegex = /^[a-zA-Z\s]*$/; //* Only letters
+const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+const phoneRegex = /^(\(\d{2}\)|\d{2})? ?\d{5}[- ]?\d{4}$/;
+
+const modal = select('.modal');
 
 const openModal = () => {
   modal.showModal();
@@ -7,3 +20,33 @@ const openModal = () => {
 const closeModal = () => {
   modal.close();
 };
+
+const countryList = select('#country'),
+  addrBookList = select('.contact__table--body'),
+  addBtn = select('.footer__save');
+      
+
+let addrName = firstName = lastName = email = phone = streetAddr = postCode =
+  city = country = labels = '';
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadJSON();
+});
+
+
+loadJSON = () => {
+  fetch('/mock/countries.json')
+    .then(res => res.json())
+    .then(data => {
+      let html = '';
+      data.forEach(({ country }) => {
+        
+        html += `
+          <option>${country}</option>
+        `;
+      });
+      console.log(html);
+      countryList.innerHTML = html;
+      console.log(countryList.innerHTML);
+    })
+}
