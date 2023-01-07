fbde49dd727cb243f0cb1791ba4a6d03a94b9f38
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-01-07 20:38:02 -0300

    fix: prevent sheet download if has no contacts

--- assets/js/export.js
+++ assets/js/export.js
@@ -1,5 +1,49 @@
+
+let myNotify;
+const displayedNotifications = [];
+
+pushNotify = (status, title, text) => {
+  const message = `${title}: ${text}`;
+  if (displayedNotifications.includes(message)) {
+    return;
+  }
+
+  displayedNotifications.push(message);
+
+  setTimeout(() => {
+    displayedNotifications.pop();
+  }, 5000);
+
+  myNotify = new Notify({
+    status: `${status}`,
+    title: `${title}`,
+    text: `${text}`,
+    effect: 'slide',
+    showIcon: true,
+    showCloseButton: true,
+    autoclose: true,
+    autotimeout: 3000,
+    distance: 20,
+    type: 1,
+  });
+}
+
+close = () => {
+  const index = displayedNotifications.indexOf(myNotify.text);
+  if (index > -1) {
+    displayedNotifications.splice(index, 1);
+  }
+  myNotify.close();
+}
+
 exportToExcel = () => {
   let table = document.querySelector('.contact__table');
+  let tbody = table.querySelector('tbody');
+
+  if (tbody.childElementCount === 0) {
+    pushNotify('error', 'Erro', 'Não há dados para exportar!');
+    return;
+  }
 
   let wb = XLSX.utils.table_to_book(table);
 
@@ -16,11 +60,11 @@ exportToExcel = () => {
   link.download = 'contacts.xlsx';
 
   link.click();
-}
+};
 
-s2ab = s => {
-  var buf = new ArrayBuffer(s.length);
-  var view = new Uint8Array(buf);
-  for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
+s2ab = (s) => {
+  let buf = new ArrayBuffer(s.length);
+  let view = new Uint8Array(buf);
+  for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
   return buf;
-}
+};
